d8b4345243461dbfa2076c106c0a34adf51f1a50
Lmpkessels <l@lmpkessels.com>
2026-09-13 13:19:40 +0200

    add: usage_err() to indicate usage errors if they arise

--- helpers/error_helpers.c
+++ helpers/error_helpers.c
@@ -1,9 +1,26 @@
+// Helper functions for error termination
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
-// Helper function for error
 void err_exit(const char *msg) 
 {
     perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void usage_err(const char *format, ...)
+{
+    va_list argList;
+
+    fflush(stdout);
+
+    fprintf(stderr, "Usage: ");
+
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
     exit(EXIT_FAILURE);
 }
--- helpers/error_helpers.h
+++ helpers/error_helpers.h
@@ -2,5 +2,6 @@
 #define ERROR_HELPERS_H
 
 void err_exit(const char *);
+void usage_err(const char *, ...);
 
 #endif
