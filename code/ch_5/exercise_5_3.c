4f18e70b5b1c616171e9f24a4a3a80e18413cdf2
Lmpkessels <l@lmpkessels.com>
2026-09-15 23:00:27 +0200

    add(exercise 5.3): correct version, writes data when not seeking

--- code/ch_5/exercise_5_3.c
+++ code/ch_5/exercise_5_3.c
@@ -4,50 +4,53 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <error_helpers.h>
 
 void err_exit(const char *);
 void usage_err(const char *, ...);
 
-// Void is incorrect
-void main(int argc, char *argv[])
+// Writes nothing when x is used, appends text when x is not used.
+int main(int argc, char *argv[])
 {
     int open_fd;
     bool x_used;
     off_t off;
     long num_bytes;
 
-    // Argc is incorrect
-    if (argc <= 2 || strcmp(argv[1], "--help") == 0) {
+    // Check for argc not being 3 and 4 which is a usage error
+    if (argc != 3 && argc != 4 || strcmp(argv[1], "--help") == 0) {
         usage_err(("%s Pathname", argv[1]));
     }
 
-    // Here i should use strcmp instead because argv[3] is a pointer
-    x_used = ((argv[3] == "x") ? (true) : (false));
+    x_used = ((argc == 4) && (strcmp(argv[3], "x") == 0)) ? (true) : (false);
     if (x_used) {
         open_fd = open(argv[1], O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR);
         if (open_fd == -1) {
             err_exit("open");
         }
     } else {
-        open_fd = open(argv[1], O_WRONLY | O_CREAT | O_APPEND | S_IRUSR | S_IWUSR);
+        // Open in append mode if "x" is not used
+        open_fd = open(argv[1], O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
         if (open_fd == -1) {
             err_exit("open");
         }
     }
 
-
     num_bytes = atoll(argv[2]);
     off = 0;
+
     for (long i = 0; i < num_bytes; i++) {
+        // When x is used nothing is written at the offset
         if (x_used) {
             if (lseek(open_fd, off, SEEK_END) == -1) {
                 err_exit("seek");
             }
         }
 
-        // Number of bytes are written instead of 1 byte at a time
-        if (write(open_fd, "x", num_bytes) == -1) {
+        // While when x is not used "x" is written one byte at a time
+        // for the given number of bytes
+        if (write(open_fd, "x", 1) == -1) {
             err_exit("write");
         }
     }
@@ -56,5 +59,5 @@ void main(int argc, char *argv[])
         err_exit("close");
     }
  
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
